13790831bfb929f2d2ca68ebb13ced067e3fda86
Luis Martinez <luisedu554@hotmail.com>
2019-03-16 17:35:18 -0500

    El frame de login ya esta acabado, en la parte superior iría la imagen
    del logo, si tan solo no la hubieran borrado .v

--- PinturilloGrupal/src/com/edu/uptc/prg3/controller/Control.java
+++ PinturilloGrupal/src/com/edu/uptc/prg3/controller/Control.java
@@ -3,15 +3,26 @@ package com.edu.uptc.prg3.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import com.edu.uptc.prg3.view.LoginFrame;
+
 public class Control implements ActionListener {
 	
-	
+	/**
+	 * Este es solo un objeto de prueba. 
+	 */
+	public Control() {
+		LoginFrame frame = new LoginFrame(this);
+	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("dar_informacion")) {
 			
 		}
+	}
+	
+	public static void main(String[] args) {
+		Control control = new Control();
 	}
 	
 }
--- PinturilloGrupal/src/com/edu/uptc/prg3/view/LoginFrame.java
+++ PinturilloGrupal/src/com/edu/uptc/prg3/view/LoginFrame.java
@@ -1,32 +1,45 @@
 package com.edu.uptc.prg3.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.Toolkit;
+import java.awt.event.ActionListener;
 
-import javax.swing.Icon;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import com.edu.uptc.prg3.controller.Control;
 
 public class LoginFrame extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private TitleImagePanel panelImg;
 	private JPanel panelLoginInf;
 	private JPanel panelOptions;
 	private JLabel lblNickName, lblPassword, lblCreateAccount;
-	private JTextField txtNickName, txtPassword;
+	private JTextField txtNickName;
+	private JPasswordField txtPassword;
 	private JButton btnEnter, btnInfo, btnHelp, btnCreateAccount;
 	
-	public LoginFrame(Control control) {
+	public LoginFrame(ActionListener actionListener) {
 		super("Pinturillo Inicio de Sesion");
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		this.setSize(500,400);
 		this.setLayout(new BorderLayout());
 		panelImg = new TitleImagePanel();
@@ -34,13 +47,18 @@ public class LoginFrame extends JFrame{
 		
 		panelLoginInf = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
+		panelLoginInf.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10),
+				"Ingreso de información de la cuenta", 
+				0, 1, new Font("Italic", 2, 15), Color.BLACK));
+		this.add(panelLoginInf, BorderLayout.CENTER);		
 		lblNickName = new JLabel("Nombre de usuario");
+		gbc.fill = GridBagConstraints.BOTH;
 		gbc.gridx = 0;
 		gbc.gridy = 0;
 		panelLoginInf.add(lblNickName, gbc);
 		
 		txtNickName = new JTextField();
-		txtNickName.setPreferredSize(new Dimension(150,50));
+		txtNickName.setPreferredSize(new Dimension(150,30));
 		gbc.gridx=1;
 		panelLoginInf.add(txtNickName, gbc);
 		
@@ -49,31 +67,62 @@ public class LoginFrame extends JFrame{
 		gbc.gridy = 1;
 		panelLoginInf.add(lblPassword, gbc);
 		
-		txtPassword = new JTextField();
-		txtPassword.setPreferredSize(new Dimension(100,50));
+		txtPassword = new JPasswordField();
+		txtPassword.setPreferredSize(new Dimension(150,30));	
 		gbc.gridx=1;
 		panelLoginInf.add(txtPassword, gbc);
 		
 		btnEnter = new JButton("Iniciar Sesion");
-		btnEnter.addActionListener(control);
+		btnEnter.addActionListener(actionListener);
 		btnEnter.setActionCommand("iniciar_sesion");
+		btnEnter.setBackground(new Color(247, 98, 85));
 		gbc.gridy = 2;
 		panelLoginInf.add(btnEnter, gbc);
-		this.add(panelLoginInf, BorderLayout.CENTER);
+		panelLoginInf.setBackground(Color.LIGHT_GRAY);
 		
-		panelOptions = new JPanel(new GridBagLayout());
+		panelOptions = new JPanel(new BorderLayout());
 		ImageIcon icon = new ImageIcon("data/Info_Icon.png");
 		btnInfo = new JButton(icon);
-		btnInfo.addActionListener(control);
-		btnInfo.setActionCommand("dar_informacion");		
-		gbc.gridx=0;
-		gbc.gridy=0;
-		panelOptions.add(btnInfo, gbc);
+		btnInfo.addActionListener(actionListener);
+		btnInfo.setActionCommand("dar_informacion");
+		btnInfo.setPreferredSize(new Dimension(48,48));
+		panelOptions.add(btnInfo, BorderLayout.EAST);
 		
+		
+		lblCreateAccount = new JLabel("No tienes una cuenta?");
+		lblCreateAccount.setHorizontalAlignment(JLabel.CENTER);
+		panelOptions.add(lblCreateAccount, BorderLayout.NORTH);
+		
+		btnCreateAccount = new JButton("Crear Cuenta");
+		btnCreateAccount.addActionListener(actionListener);
+		btnCreateAccount.setActionCommand("crear_cuenta");
+		btnCreateAccount.setBackground(new Color(247, 98, 85));
+		panelOptions.add(btnCreateAccount, BorderLayout.CENTER);
+		
+		icon = new ImageIcon("data/Help_Icon.png");
+		btnHelp = new JButton(icon);
+		btnHelp.addActionListener(actionListener);
+		btnHelp.setActionCommand("ayuda_juego");
+		btnHelp.setPreferredSize(new Dimension(48,48));
+		panelOptions.add(btnHelp, BorderLayout.WEST);
+		this.add(panelOptions, BorderLayout.SOUTH);
+		
+		this.center();
 		this.setVisible(true);
+	}	
+	
+	public void printMessagge(String message) {
+		JOptionPane.showMessageDialog( this, message, "error", JOptionPane.ERROR_MESSAGE );
 	}
 	
-	
+	public String getTxtNickName() {
+		return txtNickName.getText();
+	}
+
+	public String getTxtPassword() {
+		return txtPassword.getText();
+	}
+
 	/**
 	 * Método que centra el frame 
 	 */
