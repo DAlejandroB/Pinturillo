fa2e94ce62b6baf5673a10588e31d42d4e5ef4a4
Luis Martinez <luisedu554@hotmail.com>
2019-03-20 07:58:32 -0500

    Comandos en control agregados

--- PinturilloGrupal/src/com/edu/uptc/prg3/controller/Control.java
+++ PinturilloGrupal/src/com/edu/uptc/prg3/controller/Control.java
@@ -27,9 +27,20 @@ public class Control implements ActionListener {
 			if(info!=null) frame.closeCreateAccountDialog();
 			//llamar a este metodo frame.getNewAccountData(); verificar si es nulo, no hacer nada
 		}else if(e.getActionCommand().equals("iniciar_sesion")) {
-			frame.createProfileFrame(this, "Nombre", 50, "./data/icons/default_profile_icon.png");
-			comm.sendLoginInfo(frame.getTxtNickName(), frame.getTxtPassword());
+			//encontrar usuario usando frame.getTxtNickName(), y comparar la contraseña registrada con la 
+			//ingresada en frame.getTxtPassword(), extraer la puntuación actual del usuario encontrado
+			//si el usuario no es encontrado imprimir un mensaje con frame.printMessagge("No se ha encontrado al usuario");
+			frame.createProfileFrame(this, frame.getTxtNickName(), 50, "./data/icons/default_profile_icon.png");
+//			comm.sendLoginInfo(frame.getTxtNickName(), frame.getTxtPassword()); descomentarear
 			this.frame.dispose();
+		}else if(e.getActionCommand().equals("ver_amigos")) {
+			
+		}else if(e.getActionCommand().equals("modificar_info")) {
+			
+		}else if(e.getActionCommand().equals("eliminar_cuenta")) {
+			
+		}else if(e.getActionCommand().equals("cerrar_sesion")) {
+			
 		}
 	}
 	
