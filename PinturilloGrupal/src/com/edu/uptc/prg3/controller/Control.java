2a15f537a5dbf6ffc2e24bb45c1a61a9e9187d32
Luis Martinez <luisedu554@hotmail.com>
2019-03-20 17:58:22 -0500

    Borré algunas clases de test de servidor, Modifiqué clases de
    vista, y agregué nuevos comandos al actionListener

--- PinturilloGrupal/src/com/edu/uptc/prg3/controller/Control.java
+++ PinturilloGrupal/src/com/edu/uptc/prg3/controller/Control.java
@@ -5,16 +5,18 @@ import java.awt.event.ActionListener;
 
 import com.edu.uptc.comm.Comunicator;
 import com.edu.uptc.prg3.view.LoginFrame;
+import com.edu.uptc.prg3.view.PinturilloProfileFrame;
 import com.edu.uptc.structure.LinkedList;
 
 public class Control implements ActionListener {
-	LoginFrame frame;
+	private LoginFrame logFrame;
+	private PinturilloProfileFrame profileFrame;
 	private Comunicator comm;
 	/**
 	 * Este es solo un objeto de prueba. 
 	 */
 	public Control() {
-		frame = new LoginFrame(this);
+		logFrame = new LoginFrame(this);
 		comm = new Comunicator();
 	}
 
@@ -22,33 +24,45 @@ public class Control implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getActionCommand().equals("crear_cuenta")) {
-			frame.createAccountDialog(this);
+			logFrame.createAccountDialog(this);
 		}else if(e.getActionCommand().equals("crear_nueva_cuenta")) {
-			String []info = frame.getNewAccountData();//verificar si el nickName no esta ya en uso!!
-			comm.sendRegisterInfo(info[0], info[1], null);
-			String operationStatus = comm.recieveMessage();
-			if(operationStatus.substring(0, 3) == "/scc") {
-				//TODO comunicar la creacion del usuario al cliente
-			}else if(operationStatus.substring(0, 3) == "/wrn") {
-				//TODO comunicar que el nombre de usuario ya está ocupado
+			String []info = logFrame.getNewAccountData();
+			if(info!=null) {
+				comm.sendRegisterInfo(info[0], info[1], null);
+				String operationStatus = comm.recieveMessage();
+				if(operationStatus.substring(0, 3) == "/scc") {
+					//TODO comunicar la creacion del usuario al cliente
+				}else if(operationStatus.substring(0, 3) == "/wrn") {
+					logFrame.printMessagge("El nombre de usuario ya se encuentra en uso.");
+					//TODO comunicar que el nombre de usuario ya está ocupado
+				}else logFrame.closeCreateAccountDialog(); 
 			}
-			if(info!=null) frame.closeCreateAccountDialog();
 			//llamar a este metodo frame.getNewAccountData(); verificar si es nulo, no hacer nada
 		}else if(e.getActionCommand().equals("iniciar_sesion")) {
 			//encontrar usuario usando frame.getTxtNickName(), y comparar la contraseña registrada con la 
 			//ingresada en frame.getTxtPassword(), extraer la puntuación actual del usuario encontrado
 			//si el usuario no es encontrado imprimir un mensaje con frame.printMessagge("No se ha encontrado al usuario");
-			frame.createProfileFrame(this, frame.getTxtNickName(), 50, "./data/icons/default_profile_icon.png");
+			this.profileFrame = new PinturilloProfileFrame(this, logFrame.getTxtNickName(), 50, "./data/icons/default_profile_icon.png");
 //			comm.sendLoginInfo(frame.getTxtNickName(), frame.getTxtPassword()); descomentarear
-			this.frame.dispose();
+			this.logFrame.dispose();
 		}else if(e.getActionCommand().equals("ver_amigos")) {
-			
+			System.out.println("hola");
+			this.profileFrame.createUserFriendsFrame(this, "nickName", new LinkedList<String>());
+		}else if(e.getActionCommand().equals("amigo_seleccionado")) {
+			String selectedFriend = this.profileFrame.getSelectedFriend();
+			//encuentra al usuario seleccionado, y extrae su información, para plasmarla en un textPane
+			this.profileFrame.setFriendInfo(selectedFriend, 50, "Online");
 		}else if(e.getActionCommand().equals("modificar_info")) {
 			
 		}else if(e.getActionCommand().equals("eliminar_cuenta")) {
-			
+			if(this.profileFrame.generateQuestionDialog("Esta seguro que desea eliminar esta cuenta?"
+					+ "\nLos cambios no podrán ser revertidos")==1) {
+				//metodo de eliminar cuenta
+			}
 		}else if(e.getActionCommand().equals("cerrar_sesion")) {
-			
+			if(this.profileFrame.generateQuestionDialog("Esta seguro que desea cerrar la sesión actual?")==1){
+				//metodo para cerrar sesion
+			}
 		}
 	}
 	
--- PinturilloGrupal/src/com/edu/uptc/prg3/view/UserFriendsFrame.java
+++ PinturilloGrupal/src/com/edu/uptc/prg3/view/UserFriendsFrame.java
@@ -1,11 +1,13 @@
 package com.edu.uptc.prg3.view;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.HeadlessException;
+import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
@@ -30,24 +32,26 @@ public class UserFriendsFrame extends JFrame{
 			LinkedList<String> friendsName) throws HeadlessException {
 		super("Amigos de "+userNick);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(500,400);
+		this.setSize(500,300);
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints gbcFrame = new GridBagConstraints();
 		friendsPanel = new JPanel();
+		friendsPanel.setBackground(Color.GRAY);
+		friendsPanel.setPreferredSize(new Dimension(250,200));
 		friendsPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.gray),
 				"Amigos actuales", 
 				0, 1, new Font("Italic", 2, 15), Color.GREEN));
 		friendsUserList = new JComboBox<String>();
 		friendsUserList.addActionListener(actionListener);
 		friendsUserList.setActionCommand("amigo_seleccionado");
+		this.fillFriendsLists(friendsName);
 		friendsPanel.add(friendsUserList);
 		gbcFrame.gridx=0;
 		gbcFrame.gridy=0;
 		this.add(friendsPanel, gbcFrame);
 		
-		friendSettingsPanel = new JPanel(new GridBagLayout());
-		
-		
+		friendSettingsPanel = new JPanel(new BorderLayout());
+		friendSettingsPanel.setBackground(Color.LIGHT_GRAY);
 		friendInfoPanel = new JPanel(new GridBagLayout());
 		GridBagConstraints gbcPanel = new GridBagConstraints();
 		txtFriendInfo = new JTextPane();
@@ -61,28 +65,55 @@ public class UserFriendsFrame extends JFrame{
 		gbcPanel.gridy=1;
 		friendInfoPanel.add(lblfriendsNumber, gbcPanel);
 		
-		gbcFrame.gridx=1;
-		this.add(friendInfoPanel, gbcFrame);
+		friendSettingsPanel.add(friendInfoPanel, BorderLayout.NORTH);
 		
 		buttonsPanel = new JPanel(new GridBagLayout());
 		btnDeleteFriend = new JButton("Eliminar amigo");
 		btnDeleteFriend.addActionListener(actionListener);
 		btnDeleteFriend.setActionCommand("borrar_amigo");
+		gbcPanel.gridx=0;
+		gbcPanel.gridy=0;
+		buttonsPanel.add(btnDeleteFriend, gbcPanel);
 		
+		btnAddFriend = new JButton("Añadir Nuevo Amigo");
+		btnAddFriend.addActionListener(actionListener);
+		btnAddFriend.setActionCommand("añadir_amigo");
+		gbcPanel.gridy=1;
+		buttonsPanel.add(btnAddFriend, gbcPanel);
 		
+		friendSettingsPanel.add(buttonsPanel, BorderLayout.SOUTH);
+		
+		gbcFrame.gridx=1;
+		this.add(friendSettingsPanel, gbcFrame);
+		this.setVisible(true);
+		this.center();
 		
 	}
 	
-	public void fillFriendsLists(LinkedList<String> onlineUsers, LinkedList<String> offlineUsers) {
-		if(!onlineUsers.isEmpty()) {
-			Node<String> aux = onlineUsers.getHead();
-			while(aux!=null) {
-				friendsUserList.addItem(aux.getInfo());
-				aux = aux.getNext();
-			}
-		}
-		if(!offlineUsers.isEmpty()) {
-			Node<String> aux = offlineUsers.getHead();
+	public String getSelectedFriend() {
+		return this.friendsUserList.getSelectedItem().toString();
+	}
+	
+	public void setFriendInfo(String nickName, long globalScore, String status) {
+		String info = "NickName: "+nickName+"\n";
+			    info+="GlobalScore: "+globalScore+"\n";
+			    info+="Status: "+status;
+		this.txtFriendInfo.setText(info);
+	}
+	
+	/**
+	 * Método que centra el frame 
+	 */
+	private void center() {
+        Dimension screen = Toolkit.getDefaultToolkit( ).getScreenSize( );
+        int xEdge = ( screen.width - getWidth( ) ) / 2;
+        int yEdge = ( screen.height - getHeight( ) ) / 2;
+        setLocation( xEdge, yEdge );
+    }
+	
+	public void fillFriendsLists(LinkedList<String> friendsName) {
+		if(!friendsName.isEmpty()) {
+			Node<String> aux = friendsName.getHead();
 			while(aux!=null) {
 				friendsUserList.addItem(aux.getInfo());
 				aux = aux.getNext();
